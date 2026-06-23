0ce0e3442117e881ce2701b06d3dbabd47197324
Muser Yeung <museryeung@banamedia.com>
2026-06-23 21:18:07 +0800

    remove the migrations and db from codebase

--- nhost/migrations/default/1740000002000_events_join_code/down.sql
+++
@@ -1,6 +0,0 @@
-DROP INDEX IF EXISTS public.events_join_code_unique_idx;
-
-ALTER TABLE public.events
-  DROP COLUMN IF EXISTS join_code_rotated_at,
-  DROP COLUMN IF EXISTS qr_access_enabled,
-  DROP COLUMN IF EXISTS join_code;
